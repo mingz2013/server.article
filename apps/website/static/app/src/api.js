b4f4b14e380292e7a2b8784081c9aa96de478fd9
mingz <305603665@qq.com>
2016-11-13 02:06:25 +0800

    init

--- apps/website/static/app/src/api.js
+++ apps/website/static/app/src/api.js
@@ -1,3 +1,5 @@
 /**
  * Created by zhaojm on 12/11/2016.
  */
+
+window.alert("123");
---
+++ apps/website/static/app/src/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by zhaojm on 13/11/2016.
+ */
+require('./api.js');
+require('./services.js');
+require('../css/style.css');
