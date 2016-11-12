02287c63d3d2a5d8e5944c2d59f36ab80a0bb98f
mingz <305603665@qq.com>
2016-11-13 02:47:27 +0800

    init

--- apps/website/static/app/src/index.js
+++ apps/website/static/app/src/index.js
@@ -1,6 +1,6 @@
 /**
  * Created by zhaojm on 13/11/2016.
  */
-require('./api.js');
-require('./services.js');
-require('../css/style.css');
+import './api.js'
+import './services.js'
+import '../css/style.css'
