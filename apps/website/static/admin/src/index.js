5cec3ddce267d708c0c9e7504bb2babcf06b156a
mingz <305603665@qq.com>
2016-11-14 14:33:56 +0800

    init

--- apps/website/static/admin/src/index.js
+++ apps/website/static/admin/src/index.js
@@ -16,13 +16,28 @@ import $ from 'jQuery'
 
 $(document).ready(() => {
 
-    $('#user_list').show(function () {
-        //console.log('user_list...');
+    $('#user_list').show(() => {
         user_service.display_user_list();
     });
 
-
     $('#add_user').onclick = user_service.add_user();
+
+    $('#user_detail').show(() => {
+        user_service.get_user('{{ user_id }}');
+    });
+
+
+    $('#user_edit').onclick = () => {
+        location.href = "/admin/user/update/" + '{{ user_id }}'
+    };
+    $('#user_remove').onclick = user_service.remove_user('{{ user_id }}');
+
+
+    $('#user_update').show(() => {
+        user_service.get_user_update('{{ user_id }}');
+    });
+
+    $('#update').onclick = user_service.update_user('{{ user_id }}');
 
 
 });
