3e55aadaba107c1c66f9e30e91024378530588eb
mingz <305603665@qq.com>
2016-11-14 13:33:20 +0800

    init

--- apps/website/static/app/src/index.js
+++ apps/website/static/app/src/index.js
@@ -1,15 +1,3 @@
 /**
  * Created by zhaojm on 13/11/2016.
  */
-
-import '../css/style.css'
-
-import user_service from './services/user_service'
-import article_service from './services/article_service'
-
-import $ from 'jQuery'
-
-//window.$ = $; // TODO 没用??
-window.user_service = user_service;
-window.article_service = article_service;
-
