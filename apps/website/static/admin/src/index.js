9b936ca93f0570e8f8586d5f9f834d2b42ffc24a
mingz <305603665@qq.com>
2016-11-14 14:17:37 +0800

    init

--- apps/website/static/admin/src/index.js
+++ apps/website/static/admin/src/index.js
@@ -10,6 +10,22 @@ import article_service from './services/article_service'
 import $ from 'jQuery'
 
 //window.$ = $; // TODO 没用??
-window.user_service = user_service;
-window.article_service = article_service;
+//window.user_service = user_service;
+//window.article_service = article_service;
+
+
+$(document).ready(function () {
+
+    $('#user_list').show(function () {
+        //console.log('user_list...');
+        user_service.display_user_list();
+    });
+
+
+});
+
+
+
+
+
 
