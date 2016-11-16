8857af043fc0554ed08a1841f1f3e0d3f63ff001
mingz <305603665@qq.com>
2016-11-17 02:48:14 +0800

    init

--- apps/website/static/admin/src/controller/article/article_list_controller.js
+++ apps/website/static/admin/src/controller/article/article_list_controller.js
@@ -19,7 +19,7 @@ class ArticleListController {
                 html_str += '<tr>' +
                     '<td>' + index + '</td>' +
                     '<td><a href="/admin/article/detail/' + _id + '">' + title + '</a></td>' +
-                    '<td>' + author.username + '</td>' +
+                    '<td>' + author + '</td>' +
                     '<td><a href="/admin/article/edit/' + _id + '">edit</a></td>' +
                     '<td><a href="javascript:void(0);" onclick="window.controller.remove_article(\'' + _id + '\')">remove</a></td>' +
                     '</tr>';
